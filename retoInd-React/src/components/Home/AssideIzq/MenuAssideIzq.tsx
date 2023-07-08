9dd404f16932f1dd3e373e8a0e3b8b2d9435a1da
paoox <pao.arreola.g@gmail.com>
2023-07-08 15:00:27 -0500

    changes heigth icons

--- retoInd-React/src/components/Home/AssideIzq/MenuAssideIzq.tsx
+++ retoInd-React/src/components/Home/AssideIzq/MenuAssideIzq.tsx
@@ -2,7 +2,7 @@ export default function MenuAssideIzq(props) {
   const { icon, text } = props;
   return (
     <div className="hover:bg-slate-300/50 rounded-md p-1 flex items-center cursor-pointer">
-      <img src={icon} alt="" className="text-[12px] px-1 h-5" />
+      <img src={icon} alt="" className="text-[12px] px-1 h-4" />
       <span className="text-[10px] px-1 text-gray-600 hover:text-[#3b49df] hover:underline ">
         {text}
       </span>
