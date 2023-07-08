8cae3b6b5aec1dba3ac0d7477a4230e39c276e93
paoox <pao.arreola.g@gmail.com>
2023-07-08 01:49:48 -0500

    position navBar

--- retoInd-React/src/Pages/Home.tsx
+++ retoInd-React/src/Pages/Home.tsx
@@ -3,8 +3,9 @@ import Footer from "../components/Footer/Footer";
 
 export default function Home() {
   return (
-    <main className="bg-[#F5F5F5] min-h-screen flex flex-col text-neutral-200 ">
+    <main className="bg-[#F5F5F5] min-h-screen flex flex-col text-neutral-200  relative">
       <NavHome />
+      <section></section>
       <Footer />
     </main>
   );
