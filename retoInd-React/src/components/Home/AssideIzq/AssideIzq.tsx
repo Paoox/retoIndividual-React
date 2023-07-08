1780fb76dc3811bd5b5ef7403912224fe9bc036f
paoox <pao.arreola.g@gmail.com>
2023-07-08 03:42:27 -0500

    add MenuAssideIzq emojis

--- retoInd-React/src/components/Home/AssideIzq/AssideIzq.tsx
+++ retoInd-React/src/components/Home/AssideIzq/AssideIzq.tsx
@@ -6,6 +6,15 @@ export default function AssideIzq() {
       <MenuAssideIzq icon="🏠" text="Home" />
       <MenuAssideIzq icon="📜" text="Listings" />
       <MenuAssideIzq icon="🎙" text="Podcasts" />
+      <MenuAssideIzq icon="🎥" text="Videos" />
+      <MenuAssideIzq icon="🏷" text="Tags" />
+      <MenuAssideIzq icon="💡" text="FAQ" />
+      <MenuAssideIzq icon="🛍" text="Forem Shop" />
+      <MenuAssideIzq icon="❤" text="Sponsors" />
+      <MenuAssideIzq icon="🏳‍🌈" text="About" />
+      <MenuAssideIzq icon="📯" text="Contact" />
+      <MenuAssideIzq icon="📖" text="Guides" />
+      <MenuAssideIzq icon="🤔" text="Software comparisons" />
     </aside>
   );
 }
