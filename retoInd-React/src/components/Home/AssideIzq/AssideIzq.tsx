b0a90e397d1dde1623fcc1e4853e617b9e4c5fd5
paoox <pao.arreola.g@gmail.com>
2023-07-08 23:09:10 -0500

    finish aside izq

--- retoInd-React/src/components/Home/AssideIzq/AssideIzq.tsx
+++ retoInd-React/src/components/Home/AssideIzq/AssideIzq.tsx
@@ -21,10 +21,12 @@ import question from "../../../assets/question-Emoji-Icon.svg";
 import like from "../../../assets/like-Yellow-Hand-Icon.svg";
 import nerd from "../../../assets/nerd-Emoji-Icon.svg";
 import eyes from "../../../assets/surprised-Eyes-Icon.svg";
+import settings from "../../../assets/custumize-Tag.svg";
+import dots from "../../../assets/three-Dots-Icon.svg";
 
 export default function AssideIzq() {
   return (
-    <aside className="basis-1/4">
+    <aside className="basis-1/5">
       <MenuAssideIzq icon={house} text="Home" />
       <MenuAssideIzq icon={oldDoc} text="Listings" />
       <MenuAssideIzq icon={microp} text="Podcasts" />
@@ -37,7 +39,7 @@ export default function AssideIzq() {
       <MenuAssideIzq icon={trumpet} text="Contact" />
       <MenuAssideIzq icon={book} text="Guides" />
       <MenuAssideIzq icon={question} text="Software comparisons" />
-      <div className="rounded-md p-1 flex items-center">
+      <div className=" p-1 flex items-center">
         <span className="text-[12px] px-1 my-3 text-zinc-900 font-extrabold">
           Other
         </span>
@@ -45,8 +47,8 @@ export default function AssideIzq() {
       <MenuAssideIzq icon={like} text="Code of Conduct" />
       <MenuAssideIzq icon={nerd} text="Privacy Policy" />
       <MenuAssideIzq icon={eyes} text="Terms of use" />
-      <div className="p-2">
-        <div className="flex my-3 gap-1">
+      <div className="p-1">
+        <div className="flex mt-3 gap-2">
           <MenuIcons icon={twit} name="twitter" />
           <MenuIcons icon={face} name="facebook" />
           <MenuIcons icon={git} name="github" />
@@ -54,6 +56,49 @@ export default function AssideIzq() {
           <MenuIcons icon={twitch} name="twitch" />
           <MenuIcons icon={mastodon} name="mastodon" />
         </div>
+      </div>
+      <div className="p-1 flex justify-between items-center mx-1 mb-12">
+        <span className="text-[12px] my-3 text-zinc-900 font-extrabold">
+          My Tags
+        </span>
+        <img src={settings} alt="settings" className="h-4" />
+      </div>
+      <div className="bg-white rounded-md p-2">
+        <div className="flex gap-[38%] py-2">
+          <p className="text-zinc-600 text-[9px] ">DEV community</p>
+          <img src={dots} alt="three dots" className="h-4" />
+        </div>
+        <img
+          src="https://res.cloudinary.com/practicaldev/image/fetch/s--Iim0709g--/c_limit%2Cf_auto%2Cfl_progressive%2Cq_auto%2Cw_350/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/2tlkmy55xik6gfbcah9f.jpg"
+          alt="DEV Newsletter"
+          className="rounded-[4px] pb-2 "
+        />
+        <h2 className="text-[#404040] text-[14px] font-bold mr-6">
+          Want to stay ahead of the game in the world of coding?
+        </h2>
+        <p className="hover:text-blue-800 text-[10px] my-2 underline text-blue-600 cursor-pointer">
+          Join our newsletter community and never miss a beat.
+        </p>
+      </div>
+      <div className="bg-white rounded-md p-2">
+        <div className="flex gap-[38%] py-2">
+          <p className="text-zinc-600 text-[9px] ">DEV community</p>
+          <img src={dots} alt="three dots" className="h-4" />
+        </div>
+        <img
+          src="https://res.cloudinary.com/practicaldev/image/fetch/s--vf_P4FwD--/c_limit%2Cf_auto%2Cfl_progressive%2Cq_auto%2Cw_350/https://p32.f1.n0.cdn.getcloudapp.com/items/OAu6mnD1/20ada628-9434-4772-a8e0-7d861713d3c4.jpg"
+          alt="Dev Forem"
+          className="rounded-md my-2"
+        />
+        <h2 className="text-[#404040] text-[14px] font-bold mr-6 my-2">
+          The Forem Shop is Here!
+        </h2>
+        <p className="hover:text-blue-800 text-[10px] underline text-blue-600 cursor-pointer">
+          <code>
+            {">"}
+            {">"}Shop Forem, DEV, and CodeNewbie merch
+          </code>
+        </p>
       </div>
     </aside>
   );
--- retoInd-React/src/components/Home/MainHome.tsx
+++ retoInd-React/src/components/Home/MainHome.tsx
@@ -1,11 +1,12 @@
 import { Link } from "react-router-dom";
 import AssideIzq from "./AssideIzq/AssideIzq";
+import PostHome from "./Posts-Home/PostHome";
 
 export default function MainHome() {
   return (
     <section className="flex px-32 gap-3 my-3">
       <AssideIzq />
-      <main className="basis-[48%] bg-yellow-400">main</main>
+      <PostHome />
       <aside className="basis-[27%] bg-green-400">asside derecho</aside>
     </section>
   );
