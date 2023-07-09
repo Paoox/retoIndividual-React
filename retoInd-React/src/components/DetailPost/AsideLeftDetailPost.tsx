8318ed4660f7dc04b43fa9296ffe3cb3a9370766
paoox <pao.arreola.g@gmail.com>
2023-07-09 15:00:01 -0500

    finish aside  left detail post

--- retoInd-React/src/components/DetailPost/AsideLeftDetailPost.tsx
+++ retoInd-React/src/components/DetailPost/AsideLeftDetailPost.tsx
@@ -1,3 +1,32 @@
+import heartPlus from "../../assets/heart-plus.svg";
+import coment from "../../assets/comments-count.svg";
+import bookmark from "../../assets/read.svg";
+import dots from "../../assets/three-Dots-Icon.svg";
+
 export default function AsideLeftDetailPost() {
-  return <section className="bg-orange-500 basis-[7%]">menu izq</section>;
+  return (
+    <section className=" basis-[7%] ">
+      <div className="flex px-2 py-5 flex-col gap-2 items-center text-[10px] text-[#171313] text-center  ">
+        <div className="p-2">
+          <img
+            src={heartPlus}
+            alt="Heart plus"
+            className="h-4 fill-[#525252]"
+          />
+          <p>63</p>
+        </div>
+        <div className="p-2">
+          <img src={coment} alt="Comments" className="h-5 fill-[#525252]" />
+          <p>18</p>
+        </div>
+        <div className="p-2">
+          <img src={bookmark} alt="Bookmark" className="h-[13px]" />
+          <p>22</p>
+        </div>
+        <div className="p-2">
+          <img src={dots} alt="dots" className="h-6" />
+        </div>
+      </div>
+    </section>
+  );
 }
