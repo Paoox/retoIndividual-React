b0b0c12287a090d3b88537e2f0047826070ee621
paoox <pao.arreola.g@gmail.com>
2023-07-09 00:13:07 -0500

    add data user & post title

--- retoInd-React/src/components/Home/Posts-Home/PostHome.tsx
+++ retoInd-React/src/components/Home/Posts-Home/PostHome.tsx
@@ -1,6 +1,6 @@
 export default function PostHome() {
   return (
-    <main className="basis-[48%]">
+    <main className="basis-[55%]">
       <div className="flex text-xs gap-1 m-1">
         <div className="font-bold text-black hover:bg-white hover:rounded-md hover:text-blue-800 cursor-pointer p-2">
           Relevant
@@ -18,6 +18,26 @@ export default function PostHome() {
           alt="article DevTo"
           className="rounded-t-md cursor-pointer"
         />
+      </div>
+      <div className="flex p-3 gap-1">
+        <div className="">
+          <img
+            src="https://res.cloudinary.com/practicaldev/image/fetch/s--ifX47biZ--/c_fill,f_auto,fl_progressive,h_90,q_auto,w_90/https://dev-to-uploads.s3.amazonaws.com/uploads/user/profile_image/682769/87fdd296-6176-41b1-86a4-740375c3e6d2.jpg"
+            alt="Image User"
+            className="rounded-full h-6"
+          />
+        </div>
+        <div className="w-full">
+          <p className="text-gray-500 text-[10px]  leading-none">
+            Pradumna Saraf
+          </p>
+          <p className="text-gray-500 text-[10px]  leading-none">
+            Jul 6 (2 days ago)
+          </p>
+          <h2 className="text-black font-bold text-lg my-2 leading-tight">
+            Renaming Git Commit Messages: A Quick and Easy Guide
+          </h2>
+        </div>
       </div>
     </main>
   );
