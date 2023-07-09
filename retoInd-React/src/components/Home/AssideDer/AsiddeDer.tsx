f7dc844bcfbd800328565e85f79d28f53785a766
paoox <pao.arreola.g@gmail.com>
2023-07-09 03:17:44 -0500

    add listing aside rigth

---
+++ retoInd-React/src/components/Home/AssideDer/AsiddeDer.tsx
@@ -0,0 +1,12 @@
+export default function AssideDer() {
+  return (
+    <aside className="basis-[27%]">
+      <div className="bg-white rounded-md">
+        <div className="flex justify-between border-b border-zinc-200 p-3 items-center ">
+          <p className="text-black font-bold text-xs">Listings</p>
+          <p className="text-sky-800 text-[9px] cursor-pointer">See all</p>
+        </div>
+      </div>
+    </aside>
+  );
+}
--- retoInd-React/src/components/Home/MainHome.tsx
+++ retoInd-React/src/components/Home/MainHome.tsx
@@ -1,13 +1,13 @@
-import { Link } from "react-router-dom";
 import AssideIzq from "./AssideIzq/AssideIzq";
 import PostHome from "./Posts-Home/PostHome";
+import AssideDer from "./AssideDer/AsiddeDer";
 
 export default function MainHome() {
   return (
     <section className="flex px-32 gap-3 my-3">
       <AssideIzq />
       <PostHome />
-      <aside className="basis-[27%] bg-green-400">asside derecho</aside>
+      <AssideDer />
     </section>
   );
 }
